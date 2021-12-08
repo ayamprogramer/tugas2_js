0ee02ac3fffdbe4b0150b0c10054aa3268968106
ayamprogramer <73216195+ayamprogramer@users.noreply.github.com>
2021-12-08 13:09:05 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,14 @@
+var number = 3;
+var string = "string";
+var kosong = "null";
+kosong = null;
+var undifined;
+var boolean = true;
+console.log(number);
+console.log(string);
+console.log(kosong);
+console.log(undifined);
+console.log(boolean);
+
+
+
